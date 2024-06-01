2ec2d8e357c8773e2046d16a73910af4506d9cf3
Chris Gray <chgray@gmail.com>
2024-06-01 16:26:00 -0700

    iterate

--- Tools/merge_pdfs.sh
+++ Tools/merge_pdfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 function merge_pdf {
   if [ ! -f "$1" ]; then
@@ -8,17 +8,29 @@ function merge_pdf {
     exit 1
   fi
   
-  # echo "Adding $1 to $2"
-  pdfunite "$2" "$1" temp.pdf 
-  echo $?
+  #echo "Adding $1 to $2"
+  me=$(basename "$1")
   
+  echo "# NEW DOCUMENT MARKER" > ./header.md
+  echo "------------" >> ./header.md
+  echo . >> ./header.md
+  echo "   *Original File: $me*" >> ./header.md
+  echo . >> ./header.md
+  echo "   *Notes:* " >> ./header.md
+  echo . >> ./header.md
+  myDir=$(pwd)
+  podman run --rm -v "$myDir:/data" docker.io/chgray123/pandoc-arm:extra ./header.md -o ./Header.pdf
+  ls -l ./Header.pdf
+  pdfunite "$2" ./Header.pdf "$1" temp.pdf 
+  echo $?
   mv ./temp.pdf "$2"  
+  
+  echo "DONE"
 }
 
 
 # Start
 cp ../Participant_Notebook_Loose/PDFs/FirstPage_Binder_Owner.pdf ./WB1-69-24_ParticipantGuide.pdf
-
 
 cat Course_PDFS.txt | while read line 
 do
