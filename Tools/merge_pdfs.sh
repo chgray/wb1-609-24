fb4adcac6abad8c37307a298d39b26298f0761a2
Chris Gray <chgray@gmail.com>
2024-06-01 19:38:12 -0700

    iterating

--- Tools/merge_pdfs.sh
+++ Tools/merge_pdfs.sh
@@ -3,36 +3,64 @@
 #set -e
 
 function merge_pdf {
-  if [ ! -f "$1" ]; then
-    echo "Missing File - stopping: $1"
+  
+  IFS=';' tokens=( $1 )
+  input_file=${tokens[1]}
+  day=${tokens[0]}
+  dest_file="$day-$2"
+  
+  echo "Day : $day"
+  echo "Dest : $dest_file"
+
+  if [ ! -f "$input_file" ]; then
+    echo "Missing File - stopping: $input_file"
     exit 1
   fi
   
   #echo "Adding $1 to $2"
-  me=$(basename "$1")
+  me=$(basename "$input_file")
   
-  echo "# NEW DOCUMENT MARKER" > ./header.md
+  
+  echo "# $day" > ./header.md
+  echo  >> ./header.md
+  echo "**WB1-609-24 -- DRAFT**"  >> ./header.md
+  echo  >> ./header.md
   echo "------------" >> ./header.md
-  echo . >> ./header.md
-  echo "   *Original File: $me*" >> ./header.md
-  echo . >> ./header.md
-  echo "   *Notes:* " >> ./header.md
-  echo . >> ./header.md
+  echo  >> ./header.md
+  echo "**Original File: **" >> ./header.md
+  echo "$me" >> ./header.md
+  echo  >> ./header.md
+  echo  >> ./header.md
+  echo "**Notes:** " >> ./header.md
+  echo >> ./header.md
+  
+  
   myDir=$(pwd)
   podman run --rm -v "$myDir:/data" docker.io/chgray123/pandoc-arm:extra ./header.md -o ./Header.pdf
   ls -l ./Header.pdf
-  pdfunite "$2" ./Header.pdf "$1" temp.pdf 
-  echo $?
-  mv ./temp.pdf "$2"  
   
+  if [ ! -f "$dest_file" ]; then
+    echo "No Dest; just copy"
+    pdfunite ./Header.pdf "$input_file" temp.pdf 
+  else
+    pdfunite "$dest_file" ./Header.pdf "$input_file" temp.pdf 
+  fi
+ 
+  
+  
+  echo $?
+  mv ./temp.pdf "$dest_file"  
+  
+  #exit 1
   echo "DONE"
 }
 
 
 # Start
-cp ../Participant_Notebook_Loose/PDFs/FirstPage_Binder_Owner.pdf ./WB1-69-24_ParticipantGuide.pdf
+rm ./*.pdf
+cp ../Participant_Notebook_Loose/PDFs/FirstPage_Binder_Owner.pdf Day1-WB1-69-24_ParticipantGuide.pdf
 
 cat Course_PDFS.txt | while read line 
 do
-  merge_pdf "${line}" ./WB1-69-24_ParticipantGuide.pdf
+  merge_pdf "${line}" WB1-69-24_ParticipantGuide.pdf
 done
