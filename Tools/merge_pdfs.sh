d1cf503e1bb87941bb9587aa62051bf029e07a39
Chris Gray <chgray@gmail.com>
2024-06-01 21:17:25 -0700

    iterate

--- Tools/merge_pdfs.sh
+++ Tools/merge_pdfs.sh
@@ -59,21 +59,96 @@ function merge_pdf {
 
 # Start
 rm ./*.pdf
+rm ./*.md
 cp ../Participant_Notebook_Loose/PDFs/FirstPage_Binder_Owner.pdf Day1-WB1-69-24_ParticipantGuide.pdf
 
+#
+#  Build up a header for the section
+#
 cat Course_PDFS.txt | while read line 
 do
-
   IFS=';' tokens=( $line )
   input_file=${tokens[1]}
   day=${tokens[0]}
   dest_file="$day-WB1-69-24_ParticipantGuide.pdf"
+  header_name="$day-Header.md"
+  
+  echo "HeaderName $header_name"
+  
+  if [ ! -f "$header_name" ]; then
+    echo "Missing File - stopping: $input_file"
+    
+    echo "# $day" > $header_name
+    echo  >> $header_name
+    echo "**WB1-609-24 -- DRAFT**"  >> $header_name
+    echo  >> $header_name
+    echo "------------" >> $header_name
+    echo  >> $header_name
+    echo "**Original File: **"  >> $header_name
+    echo  >> $header_name
+    echo   >> $header_name
+    echo " | Section | File | " >> $header_name
+    echo  " | --| -- | " >> $header_name
+  fi
+  
+  echo $input_file | gawk '{split($0, bits, "/"); print("|" bits[5] "|" bits[6] "|");}' >>  $header_name
   
   if [ ! -f "$input_file" ]; then
     echo "Missing File - stopping: $input_file"
     exit 1
   fi
+  
+  echo $day
+  echo $input_file
+  echo "----"
+done
 
+
+#
+# Start each file with just the header
+# 
+cat Course_PDFS.txt | while read line 
+do
+  IFS=';' tokens=( $line )
+  input_file=${tokens[1]}
+  day=${tokens[0]}
+  dest_file="$day-Header.pdf"
+  header_name="$day-Header.md"  
+  
+  if [ ! -f "$input_file" ]; then
+    echo "Missing File - stopping: $input_file"
+    exit 1
+  fi
+  
+  echo "Header : $header_name"
+  echo "Dest : $dest_file"
+  
+  make_header_myDir=$(pwd)
+  podman run --rm -v "$make_header_myDir:/data" docker.io/chgray123/pandoc-arm:extra $header_name -o $dest_file.pdf 
+  exit
+done
+
+
+
+
+cat Course_PDFS.txt | while read line 
+do
+  IFS=';' tokens=( $line )
+  input_file=${tokens[1]}
+  day=${tokens[0]}
+  dest_file="$day-WB1-69-24_ParticipantGuide.pdf"
+  main_header="$day-Header.pdf"
+    
+  if [ ! -f "$input_file" ]; then
+    echo "Missing File - stopping: $input_file"
+    exit 1
+  fi
+  
+  
+  if [ ! -f "$dest_file" ]; then
+    cp $main_header $dest_file
+  fi
+  
   echo "InputFile : $input_file"
   make_header $day $input_file $dest_file
   echo "Merging Header"
