cd23c786e89f9460d52058bea6a8926389c0b6b5
Chris Gray <chgray@gmail.com>
2024-06-02 18:58:27 -0700

    iterate

--- Participant_Notebook_Loose/RenderDocs2PDF.cmd
+++ Participant_Notebook_Loose/RenderDocs2PDF.cmd
@@ -8,3 +8,7 @@ echo --
 echo "winget install TobyAllen.DocTo"
 
 docto -WD -f FirstPage_Binder_Owner.docx -o .\PDFs\FirstPage_Binder_Owner.pdf -t wdFormatPDF
+docto -WD -f MapOfPigott.docx -o .\PDFs\MapOfPigott.pdf -t wdFormatPDF
+docto -WD -f EmergencyAndSafetyInfo.docx -o .\PDFs\EmergencyAndSafetyInfo.pdf -t wdFormatPDF
+docto -WD -f PatrolRoster.docx -o .\PDFs\PatrolRoster.pdf -t wdFormatPDF
+docto -WD -f Staff.docx -o .\PDFs\Staff.pdf -t wdFormatPDF
